3cc03664ab66345c5a035f390fea2d3575728bf0
Berkay Yelken <berkayelken@gmail.com>
2024-11-25 13:46:07 +0300

    init commit

---
+++ src/main/java/io/github/berkayelken/advocate/telegram/bot/AdvocateTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package io.github.berkayelken.advocate.telegram.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvocateTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvocateTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/berkayelken/advocate/telegram/bot/AdvocateTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.berkayelken.advocate.telegram.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvocateTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
