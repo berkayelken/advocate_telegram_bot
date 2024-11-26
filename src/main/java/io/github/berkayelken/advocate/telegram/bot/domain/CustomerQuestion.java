5f0821c0e8f700283f5944fb9b2acb72f94a6a05
Berkay Yelken <berkayelken@gmail.com>
2024-11-27 01:28:19 +0300

    renaming

--- src/main/java/io/github/berkayelken/advocate/telegram/bot/domain/CustomerQuestion.java
+++ src/main/java/io/github/berkayelken/advocate/telegram/bot/domain/CustomerQuestion.java
@@ -9,10 +9,20 @@ import org.springframework.data.mongodb.core.mapping.MongoId;
 @Getter
 @Setter
 @Document
-public class StandaloneCustomerQuestion extends CustomerQuestion {
+public class CustomerQuestion {
 	@Id
 	@MongoId
 	private String id;
 	private String email;
 	private String phone;
+	private String question;
+	private String category;
+	private String issueType;
+	private String answer;
+	private String abstractAnswer;
+	private String abstractQuestion;
+	private String lawyerAnswer;
+	private long askingTime;
+	private boolean askedToAI;
+	private boolean seenByCustomer;
 }
--- src/test/java/io/github/berkayelken/advocate/telegram/bot/domain/CustomerQuestionTest.java
+++ src/test/java/io/github/berkayelken/advocate/telegram/bot/domain/CustomerQuestionTest.java
@@ -3,24 +3,22 @@ package io.github.berkayelken.advocate.telegram.bot.domain;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class StandaloneCustomerQuestionTest {
+public class CustomerQuestionTest {
 	private static final String TEST_STR = "TEST";
 	private static final long DEFAULT_LONG = 0L;
 
 	@Test
 	public void testNoArgsConstructor() {
-		Assertions.assertDoesNotThrow(StandaloneCustomerQuestion::new);
+		Assertions.assertDoesNotThrow(CustomerQuestion::new);
 	}
 
 	@Test
 	public void testAccessors() {
-		StandaloneCustomerQuestion question = new StandaloneCustomerQuestion();
+		CustomerQuestion question = new CustomerQuestion();
 
 		Assertions.assertNull(question.getId());
 		Assertions.assertNull(question.getEmail());
 		Assertions.assertNull(question.getPhone());
-
-		Assertions.assertNull(question.getQuestionId());
 		Assertions.assertNull(question.getQuestion());
 		Assertions.assertNull(question.getAbstractQuestion());
 		Assertions.assertNull(question.getCategory());
@@ -35,13 +33,11 @@ public class StandaloneCustomerQuestionTest {
 
 	@Test
 	public void testMutators() {
-		StandaloneCustomerQuestion question = getStandaloneCustomerQuestion();
+		CustomerQuestion question = getStandaloneCustomerQuestion();
 
 		Assertions.assertEquals(TEST_STR, question.getId());
 		Assertions.assertEquals(TEST_STR, question.getEmail());
 		Assertions.assertEquals(TEST_STR, question.getPhone());
-
-		Assertions.assertEquals(TEST_STR, question.getQuestionId());
 		Assertions.assertEquals(TEST_STR, question.getQuestion());
 		Assertions.assertEquals(TEST_STR, question.getAbstractQuestion());
 		Assertions.assertEquals(TEST_STR, question.getCategory());
@@ -54,14 +50,13 @@ public class StandaloneCustomerQuestionTest {
 		Assertions.assertEquals(Long.MAX_VALUE, question.getAskingTime());
 	}
 
-	private StandaloneCustomerQuestion getStandaloneCustomerQuestion() {
-		StandaloneCustomerQuestion question = new StandaloneCustomerQuestion();
+	private CustomerQuestion getStandaloneCustomerQuestion() {
+		CustomerQuestion question = new CustomerQuestion();
 
 		question.setId(TEST_STR);
 		question.setEmail(TEST_STR);
 		question.setPhone(TEST_STR);
 
-		question.setQuestionId(TEST_STR);
 		question.setQuestion(TEST_STR);
 		question.setAbstractQuestion(TEST_STR);
 		question.setCategory(TEST_STR);
