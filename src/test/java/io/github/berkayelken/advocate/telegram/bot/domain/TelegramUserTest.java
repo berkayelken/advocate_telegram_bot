fb95195c5e4e023a00e7932fc57f9c01b5ab6fc9
Berkay Yelken <berkayelken@gmail.com>
2024-11-27 01:15:38 +0300

    Update TelegramUserTest.java

--- src/test/java/io/github/berkayelken/advocate/telegram/bot/domain/TelegramUserTest.java
+++ src/test/java/io/github/berkayelken/advocate/telegram/bot/domain/TelegramUserTest.java
@@ -3,6 +3,8 @@ package io.github.berkayelken.advocate.telegram.bot.domain;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+
 public class TelegramUserTest {
 	private static final long DEFAULT_LONG = 0L;
 	private static final String TEST_STR = "TEST";
@@ -22,6 +24,9 @@ public class TelegramUserTest {
 		TelegramUser user = new TelegramUser();
 		Assertions.assertEquals(DEFAULT_LONG, user.getChatId());
 		Assertions.assertEquals(DEFAULT_LONG, user.getConsentTime());
+		Assertions.assertNull(user.getAllQuestions());
+		Assertions.assertNull(user.getAiQuestions());
+		Assertions.assertFalse(user.isAiEnabled());
 		Assertions.assertNull(user.getEmail());
 		Assertions.assertNull(user.getPhone());
 	}
@@ -32,11 +37,17 @@ public class TelegramUserTest {
 
 		user.setChatId(Long.MAX_VALUE);
 		user.setConsentTime(Long.MAX_VALUE);
+		user.setAiQuestions(Collections.emptyList());
+		user.setAllQuestions(Collections.emptyList());
+		user.setAiEnabled(true);
 		user.setEmail(TEST_STR);
 		user.setPhone(TEST_STR);
 
 		Assertions.assertEquals(Long.MAX_VALUE, user.getChatId());
 		Assertions.assertEquals(Long.MAX_VALUE, user.getConsentTime());
+		Assertions.assertNotNull(user.getAllQuestions());
+		Assertions.assertNotNull(user.getAiQuestions());
+		Assertions.assertTrue(user.isAiEnabled());
 		Assertions.assertEquals(TEST_STR,user.getEmail());
 		Assertions.assertEquals(TEST_STR,user.getPhone());
 	}
