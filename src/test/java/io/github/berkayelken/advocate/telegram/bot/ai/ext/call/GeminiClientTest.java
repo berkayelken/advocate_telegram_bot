ff584279f2a26a86b37d2d5812f5a49c8339f592
Berkay Yelken <berkayelken@gmail.com>
2024-11-26 20:06:42 +0300

    Update GeminiClientTest.java

--- src/test/java/io/github/berkayelken/advocate/telegram/bot/ai/ext/call/GeminiClientTest.java
+++ src/test/java/io/github/berkayelken/advocate/telegram/bot/ai/ext/call/GeminiClientTest.java
@@ -1,14 +1,19 @@
 package io.github.berkayelken.advocate.telegram.bot.ai.ext.call;
 
+import io.github.berkayelken.advocate.telegram.bot.ai.ext.call.domain.request.GeminiRequest;
+import io.github.berkayelken.advocate.telegram.bot.ai.ext.call.domain.response.GeminiResponse;
 import io.github.berkayelken.advocate.telegram.bot.properties.GeminiProperties;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class GeminiClientTest {
+	private static final String TEST_STR = "TEST";
 	@Mock
 	private GeminiProperties properties;
 	@Mock
@@ -21,7 +26,15 @@ public class GeminiClientTest {
 	@Test
 	public void testAsk() {
 		GeminiClient client = getClient();
-		Assertions.assertDoesNotThrow(() -> client.askAI("TEST"));
+		Mockito.when(properties.getApiKey()).thenReturn(TEST_STR);
+		Mockito.when(feignClient.callAI(ArgumentMatchers.anyString(), ArgumentMatchers.any(GeminiRequest.class))).thenReturn(new GeminiResponse());
+		Assertions.assertEquals(GeminiResponse.NO_RESPONSE, client.askAI(TEST_STR));
+	}
+
+	@Test
+	public void testAskWithNoResponse() {
+		GeminiClient client = getClient();
+		Assertions.assertEquals(GeminiResponse.NO_RESPONSE, client.askAI(TEST_STR));
 	}
 
 }
