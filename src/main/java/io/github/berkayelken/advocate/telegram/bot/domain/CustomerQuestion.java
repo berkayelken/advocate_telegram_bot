697ec0158b8621cc4a2c9da2c4d5d1688e506adb
Berkay Yelken <berkayelken@gmail.com>
2024-11-26 20:42:26 +0300

    Create CustomerQuestion.java

---
+++ src/main/java/io/github/berkayelken/advocate/telegram/bot/domain/CustomerQuestion.java
@@ -0,0 +1,20 @@
+package io.github.berkayelken.advocate.telegram.bot.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CustomerQuestion {
+	private String question;
+	private String category;
+	private String issueType;
+	private String answer;
+	private String abstractAnswer;
+	private String abstractQuestion;
+	private String lawyerAnswer;
+	private long askingTime;
+	private boolean seenByCustomer;
+}
