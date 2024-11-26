0368b709b35d986e9648084ebaf60b1e314b901f
Berkay Yelken <berkayelken@gmail.com>
2024-11-26 17:28:34 +0300

    feign client enabling

--- src/main/java/io/github/berkayelken/advocate/telegram/bot/AdvocateTelegramBotApplication.java
+++ src/main/java/io/github/berkayelken/advocate/telegram/bot/AdvocateTelegramBotApplication.java
@@ -2,7 +2,9 @@ package io.github.berkayelken.advocate.telegram.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class AdvocateTelegramBotApplication {
 
