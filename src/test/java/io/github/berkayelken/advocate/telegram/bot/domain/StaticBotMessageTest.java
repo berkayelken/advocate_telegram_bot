2ee6ffdaa184a1b7fa9e9892a923b0e975b6fac8
Berkay Yelken <berkayelken@gmail.com>
2024-11-27 15:43:05 +0300

    Update StaticBotMessageTest.java

--- src/test/java/io/github/berkayelken/advocate/telegram/bot/domain/StaticBotMessageTest.java
+++ src/test/java/io/github/berkayelken/advocate/telegram/bot/domain/StaticBotMessageTest.java
@@ -17,6 +17,7 @@ public class StaticBotMessageTest {
 	public void testAccessors() {
 		StaticBotMessage message = new StaticBotMessage();
 		Assertions.assertNull(message.getId());
+		Assertions.assertNull(message.getAction());
 		Assertions.assertNull(message.getMessage());
 		Assertions.assertNull(message.getFailMessage());
 		Assertions.assertNull(message.getUserMessage());
@@ -28,12 +29,14 @@ public class StaticBotMessageTest {
 	public void testMutators() {
 		StaticBotMessage message = new StaticBotMessage();
 		message.setId(TEST_STR);
+		message.setAction(MessageAction.AGREE);
 		message.setMessage(TEST_STR);
 		message.setFailMessage(TEST_STR);
 		message.setUserMessage(TEST_STR);
 		message.setPrefix(true);
 		message.setFixedKeyboard(Collections.singletonList(TEST_STR));
 		Assertions.assertEquals(TEST_STR, message.getId());
+		Assertions.assertEquals(MessageAction.AGREE, message.getAction());
 		Assertions.assertEquals(TEST_STR, message.getMessage());
 		Assertions.assertEquals(TEST_STR, message.getFailMessage());
 		Assertions.assertEquals(TEST_STR, message.getUserMessage());
