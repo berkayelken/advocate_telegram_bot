95a67f7aa3ef1451133897f5064bbbeb573bf631
Berkay Yelken <berkayelken@gmail.com>
2024-11-27 18:10:22 +0300

    Update CustomerQuestionRepository.java

--- src/main/java/io/github/berkayelken/advocate/telegram/bot/repository/CustomerQuestionRepository.java
+++ src/main/java/io/github/berkayelken/advocate/telegram/bot/repository/CustomerQuestionRepository.java
@@ -1,17 +1,25 @@
 package io.github.berkayelken.advocate.telegram.bot.repository;
 
 import io.github.berkayelken.advocate.telegram.bot.domain.CustomerQuestion;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CustomerQuestionRepository extends CrudRepository<CustomerQuestion, String> {
+public interface CustomerQuestionRepository extends MongoRepository<CustomerQuestion, String> {
 	List<CustomerQuestion> findAllByEmail(String email);
 
 	List<CustomerQuestion> findAllByEmailAndAskedToAIAndAskingTimeGreaterThanEqual(String email, boolean askedToAI,
 			long askingTime);
 
 	boolean deleteAllByEmail(String email);
+
+	Page<CustomerQuestion> findAllByCategory(Pageable pageable, String category);
+
+	Page<CustomerQuestion> findAllByIssueType(Pageable pageable, String issueType);
+
+	Page<CustomerQuestion> findAllBy(Pageable pageable);
 }
