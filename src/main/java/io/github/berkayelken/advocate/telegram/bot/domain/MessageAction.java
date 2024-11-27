07fe819e827ed516b01a1cfa4e136fc7d35c3039
Berkay Yelken <berkayelken@gmail.com>
2024-11-27 15:41:31 +0300

    Create MessageAction.java

---
+++ src/main/java/io/github/berkayelken/advocate/telegram/bot/domain/MessageAction.java
@@ -0,0 +1,39 @@
+package io.github.berkayelken.advocate.telegram.bot.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum MessageAction {
+	START(false),
+	AGREE,
+	DISAGREE(false),
+	ENABLE_AI,
+	DISABLE_AI,
+	EMAIL,
+	PHONE,
+	EMAIL_APPROVE,
+	GET_ALL_QUESTIONS,
+	GET_QUESTION,
+	DELETE_QUESTION,
+	DELETE_ACCOUNT,
+	DELETE_ACCOUNT_APPROVE,
+	DELETE_ALL_QUESTIONS,
+	HELP(false),
+	ANSWER(true, true);
+
+	private final boolean needAction;
+	private final boolean messageTemplate;
+
+	MessageAction() {
+		needAction = true;
+		messageTemplate = false;
+	}
+
+	MessageAction(boolean needAction) {
+		this.needAction = needAction;
+		messageTemplate = false;
+	}
+
+}
