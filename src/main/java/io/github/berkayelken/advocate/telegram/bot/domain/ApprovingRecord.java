c94c6477481b75380eb08d9fb1bf479800cc6347
Berkay Yelken <berkayelken@gmail.com>
2024-11-27 18:25:17 +0300

    Create ApprovingRecord.java

---
+++ src/main/java/io/github/berkayelken/advocate/telegram/bot/domain/ApprovingRecord.java
@@ -0,0 +1,19 @@
+package io.github.berkayelken.advocate.telegram.bot.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Getter
+@Setter
+@Document
+public class ApprovingRecord {
+	@Id
+	@MongoId
+	private String id;
+	private String email;
+	private String code;
+	private ApproveAction type;
+}
