d04f9ba059e9879501778a9a06db487b75a0abcf
Berkay Yelken <berkayelken@gmail.com>
2024-11-28 00:27:52 +0300

    user not found message was added

--- src/main/java/io/github/berkayelken/advocate/telegram/bot/exception/UserNotFoundException.java
+++ src/main/java/io/github/berkayelken/advocate/telegram/bot/exception/UserNotFoundException.java
@@ -1,4 +1,8 @@
 package io.github.berkayelken.advocate.telegram.bot.exception;
 
-public class UserNotFoundException extends RuntimeException{
+import lombok.Getter;
+
+@Getter
+public class UserNotFoundException extends RuntimeException {
+	private final String message = "Kullan\u0131c\u0131 kayd\u0131n\u0131z bulunamad\u0131. L\u00fctfen /start yazarak KVKK onay\u0131 ve kay\u0131t i\u015flemlerinizi yap\u0131n\u0131z.";
 }
