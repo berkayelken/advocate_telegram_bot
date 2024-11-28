ddf3ce3a93d72089e5e0037e763f8ed2bb2bbb85
Berkay Yelken <berkayelken@gmail.com>
2024-11-28 16:12:38 +0300

    Update ApprovingRecordRepository.java

--- src/main/java/io/github/berkayelken/advocate/telegram/bot/repository/ApprovingRecordRepository.java
+++ src/main/java/io/github/berkayelken/advocate/telegram/bot/repository/ApprovingRecordRepository.java
@@ -5,7 +5,9 @@ import io.github.berkayelken.advocate.telegram.bot.domain.ApprovingRecord;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ApprovingRecordRepository extends CrudRepository<ApprovingRecord, String> {
-	ApprovingRecord findByEmailAndCodeAndType(String email, String code, ApproveAction type);
+	Optional<ApprovingRecord> findByEmailAndCodeAndType(String email, String code, ApproveAction type);
 }
